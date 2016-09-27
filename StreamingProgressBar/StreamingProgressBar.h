a4e77a3fb84c17c1ef325db81bd49f742203fa30
Kyle Zaragoza <popwarsweet@gmail.com>
2016-09-27 23:01:14 +0700

    adds carthage support

---
+++ StreamingProgressBar/StreamingProgressBar.h
@@ -0,0 +1,19 @@
+//
+//  StreamingProgressBar.h
+//  StreamingProgressBar
+//
+//  Created by Kyle Zaragoza on 9/27/16.
+//  Copyright © 2016 Kyle Zaragoza. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for StreamingProgressBar.
+FOUNDATION_EXPORT double StreamingProgressBarVersionNumber;
+
+//! Project version string for StreamingProgressBar.
+FOUNDATION_EXPORT const unsigned char StreamingProgressBarVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <StreamingProgressBar/PublicHeader.h>
+
+
